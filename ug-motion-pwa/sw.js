4e5cc7f754b9ca4793fcc1cb1f30cb4fa902cb8f
urirlcuevas-ship-it <urirlcuevas@gmail.com>
2026-06-13 22:41:46 -0600

    Add files via upload

---
+++ ug-motion-pwa/sw.js
@@ -0,0 +1,4 @@
+const CACHE='ug-motion-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
